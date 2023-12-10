c5263eca4a03910056d550535a0c11318b7eb970
Ashok kr swami <ashokkrswami@gmail.com>
2023-12-10 20:35:29 +0530

    fina post
    fina post

--- Exercise 12/index.js
+++ Exercise 12/index.js
@@ -1,9 +1,9 @@
 // random color generator function
 function grc() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 15)];
     }
     return color;
   }
--- rough.js
+++ rough.js
@@ -1,5 +1,15 @@
-alert("Hello world !")
-let a = prompt("Enter first number")
-let b = prompt("Enter second number")
-let sum = a+b;
-console.log(`Your number is :- `,sum)
+// alert("Hello world !")
+// let a = prompt("Enter first number")
+// let b = prompt("Enter second number")
+// let sum = a+b;
+// console.log(`Your number is :- `,sum)
+
+let letters = '0123456789ABCDEF';
+    let color;
+    for (var i = 10; i >= 1; i--) {
+      color += ("*");
+      console.log(color);
+    }
+    //let a = Math.floor();
+    //let b = Math.random();
+    //console.log(color,a,b*16)
