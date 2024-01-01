af1373227ffcc6397cc2d050ec6a0008dc1d6de3
Ashok kr swami <ashokkrswami@gmail.com>
2024-01-01 20:40:00 +0530

    New Exercise 14 ready
    New Exercise Add 14

--- rough.js
+++ rough.js
@@ -4,12 +4,49 @@
 // let sum = a+b;
 // console.log(`Your number is :- `,sum)
 
-let letters = '0123456789ABCDEF';
-    let color;
-    for (var i = 10; i >= 1; i--) {
-      color += ("*");
-      console.log(color);
-    }
-    //let a = Math.floor();
-    //let b = Math.random();
-    //console.log(color,a,b*16)
+// let letters = '0123456789ABCDEF';
+//     let color;
+//     for (var i = 10; i >= 1; i--) {
+//       color += ("*");
+//       console.log(color);
+//     }
+//     //let a = Math.floor();
+// //     //let b = Math.random();
+// //     //console.log(color,a,b*16)
+
+
+// // create object
+
+// let ashokswami = {
+//   Fname: "Ashok",
+//   Lname: "Swami",
+//   age: 29,
+//   class: "12th"
+
+// }
+
+// //create object constructor
+// function students(fName,lastName,age,Sclass,country){
+//   this.fName = fName;
+//   this.lastName = lastName;
+//   this.age = age;
+//   this.class = Sclass;
+//   this.country = country;
+
+// }
+
+// let ashok = new students("ashok","swami",29,"12th","india");
+// let rahul = new students("rahul","singh",25,"11th","india");
+// let ashok1 = new students("ashok1","swami1",291,"112th","india1");
+// let rahul1 = new students("rahul1","singh1",251,"111th","india1");
+
+
+// console.log(ashok);
+// console.log(rahul);
+// console.log(ashok1);
+// console.log(typeof(rahul1));
+// console.log(ashokswami);
+
+
+rendumNumbre = (Math.random()*6+2);
+console.log(rendumNumbre);
